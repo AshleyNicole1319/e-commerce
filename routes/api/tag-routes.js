499048b8838586b13ace7cea6b6974fe4f0326b4
AshleyNicole1319 <a-warford96@comcast.net>
2022-10-15 11:57:56 -0400

    update tag name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,7 +61,22 @@ router.post('/', (req, res) => {
 
 //Updatae a Tag's Name by its ID Value
 router.put('/:id', (req, res) => {
-
+  Tag.update(req.body, {
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData[0]) {
+      res.status(404).json ({ message: 'No tag found.'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 //Delete Tag by its ID Value
