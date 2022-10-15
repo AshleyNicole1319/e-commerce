7587fb480fe692dcacea7d73027d4577df0e6946
AshleyNicole1319 <a-warford96@comcast.net>
2022-10-15 11:52:28 -0400

    find all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+//Find All Tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id','tag_name'],
+    include: [{
+      model: Product,
+      attributes: ['id','product_name','price','stock','category_id'],
+      as: 'tags'
+    }]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+//Find Single Tag by ID
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+
 });
 
+//Create a New Tag
 router.post('/', (req, res) => {
-  // create a new tag
+
 });
 
+//Updatae a Tag's Name by its ID Value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+
 });
 
+//Delete Tag by its ID Value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  
 });
 
 module.exports = router;
