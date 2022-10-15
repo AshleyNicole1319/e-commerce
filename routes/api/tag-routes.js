0eb73609dbb72e69295d995cd44ceae8c0ced50e
AshleyNicole1319 <a-warford96@comcast.net>
2022-10-15 11:58:49 -0400

    delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -81,7 +81,22 @@ router.put('/:id', (req, res) => {
 
 //Delete Tag by its ID Value
 router.delete('/:id', (req, res) => {
-
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData){
+      res.status(404).json({message: 'No tag found with this id.'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch (err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
